41387c5865fb240768a8fb9b16492079e3632453
samirvithlani <samir.vithlani83955@gmail.com>
2024-08-26 19:51:35 +0530

    loop

---
+++ loops/fordemo1.js
@@ -0,0 +1,10 @@
+var sp = 1
+var ep = 20
+
+for(let i=sp;i<=ep;i++){
+    console.log(i);
+}
+
+//sum of every number 1 --> 20 --> sum
+//print only even numbers bw sp to ep
+//print sum of even number and odd number bw sp to ep
