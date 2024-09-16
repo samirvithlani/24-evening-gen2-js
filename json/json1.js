d5225c759515e7eadb73a0db4f4fae669abd6e7d
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-16 19:57:10 +0530

    json

---
+++ json/json1.js
@@ -0,0 +1,43 @@
+// var id =101
+// var name = "Raj"
+// var age = 21
+// //..
+// console.log("Id is ",id)
+
+// var student = {
+//     id:101,
+//     name:"Raj",
+//     age:21,
+// }
+
+var student = {
+  id: 101,
+  name: "Raj",
+  age: 21,
+  address: {
+    city: "Ahmedabad",
+    state: "Gujarat",
+    pincode: 380009,
+    street: "Surbhi Complex",
+  },
+};
+
+console.log(student);
+console.log(student.id);
+console.log(student.name);
+console.log(student.age);
+
+console.log(student.address);
+console.log(student.address.city);
+console.log(student.address.state);
+console.log(student.address.pincode);
+console.log(student.address.street);
+// console.log(student.city);
+// console.log(student.state);
+// console.log(student.pincode);
+// console.log(student.street);
+
+
+//product --> 5 properties
+//univercity --> 10 properties
+//own ->
---
+++ string/string7.js
@@ -0,0 +1,32 @@
+var data = "ramayana"
+
+//slice substirng
+
+//var x = data.slice(0,3)// 0 1 2
+//var x = data.slice(2,6)//2 3 4 5
+//var x = data.slice(2) // till end
+//var x  =data.substring(0,3) // 0 1 2
+//var x = data.substring(2,6) // 2 3 4 5
+var x = data.substring(2) // till end
+
+console.log(x)
+
+
+var r = data.repeat(3)
+console.log(r)
+
+
+var address = "Surbhi Complex, 2nd and 3rd floor, Chimanlal Girdharlal Rd, Opposite Municipal Market, Vasant Vihar, Navrangpura, Ahmedabad, Gujarat 380009"
+
+var add = address.split(" ")
+console.log(add)
+
+
+
+
+
+
+
+
+
+
