cd28d781206cd93f9c288180a61e752b7bf1610c
samirvithlani <samir.vithlani83955@gmail.com>
2024-09-23 19:44:15 +0530

    functions

---
+++ functions/funciton1.js
@@ -0,0 +1,31 @@
+//function name(param){}
+
+//without parameter //without return
+function add(){
+    let a =100, b=200,c;
+    c = a+b;
+    console.log('Addition',c);
+}
+
+add(); //calling function
+
+
+function greetings(){
+
+    var time ="12:00";
+    if(time >= "00:00" && time < "12:00"){
+        console.log('Good Morning');
+    }
+    else if(time >= "12:00" && time < "16:00"){
+        console.log('Good Afternoon');
+    }
+    else if(time >= "16:00" && time < "21:00"){
+        console.log('Good Evening');
+    }
+    else{
+        console.log('Good Night');
+    }
+
+
+}
+greetings();
