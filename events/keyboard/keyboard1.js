12abe239bd1b0c0be0d1a2d3b9e67b6c49488bd3
samirvithlani <samir.vithlani83955@gmail.com>
2024-11-18 20:02:52 +0530

    validation

--- events/keyboard/keyboard1.js
+++ events/keyboard/keyboard1.js
@@ -8,6 +8,20 @@ const getData = (event)=>{
     console.log(txt.value);
 
     const nameValue = document.getElementById('nameValue') //<span id="nameValue"></span>
-    nameValue.innerHTML = txt.value;
+    //nameValue.innerHTML = txt.value;
+
+    if(txt.value.trim().length<=3){
+        const nameError = document.getElementById('nameError') //<span id="nameError"></span>
+        nameError.innerHTML = 'Name must be at least 3 characters';
+        nameValue.innerHTML = '';
+        txt.style.outline = '1px solid red';
+    }
+    else{
+        const nameError = document.getElementById('nameError') //<span id="nameError"></span>
+        nameError.innerHTML = '';
+        nameValue.innerHTML = txt.value;
+        txt.style.outline = '1px solid green';
+    }
+
     
 }
