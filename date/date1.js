ea753e1cf32dd58aa9b2bed5b4d5a3ede035f8ee
samirvithlani <samir.vithlani83955@gmail.com>
2025-01-27 19:46:15 +0530

    date

---
+++ date/date1.js
@@ -0,0 +1,28 @@
+//Date class
+//object...
+//new keyword
+//Date() class
+//date --> object it can be any 
+var date = new Date() 
+
+date.setFullYear(2025)
+date.setMonth(1)
+date.setDate(14)
+date.setHours(0)
+date.setMinutes(0)
+date.setSeconds(0)
+
+
+console.log(date)
+
+var date1 = new Date()
+console.log(date1)
+console.log(date1.getFullYear())
+console.log(date1.getMonth())
+console.log(date1.getDate())
+console.log(date1.getHours())
+console.log(date1.getMinutes())
+console.log(date1.getSeconds())
+//1970 1st jan 00;00;00
+console.log(date1.getTime()) //milles... 
+
